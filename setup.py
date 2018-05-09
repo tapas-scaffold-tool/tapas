a4e6d513fae40db8659201f0fe31749b65bc3d8a
Kirill Sulim <kirillsulim@gmail.com>
2018-05-09 21:29:17 +0500

    Create skeleton of app

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+
+setup(
+    name='tapas',
+    version='0.1.0',
+    description='A simple general purpose scaffold tool',
+    url='https://github.com/tapas-scaffold-tool/tapas',
+    author='Kirill Sulim',
+    author_email='kirillsulim@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='scaffold build generator generate',
+    python_requires='>=3',
+    packages=[
+        'tapas'
+    ],
+    entry_points={
+        'console_scripts': [
+            'tapas=tapas.app:main',
+        ],
+    },
+)
