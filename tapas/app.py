95f1cab19f7c5f3e02ab3a5b57acfff8688db038
Kirill Sulim <kirillsulim@gmail.com>
2018-05-11 21:02:41 +0500

    Add config file handler

--- tapas/app.py
+++ tapas/app.py
@@ -4,19 +4,22 @@ from os import path as path
 from jinja2 import Environment, Template, FileSystemLoader, Undefined, StrictUndefined
 
 
+TAPAS_FILE = 'conf.py'
+
+
 @click.command()
 @click.argument('tapa')
 def main(tapa):
-    params = _get_params()
-    _walk(_get_path(tapa), os.getcwd(), params)
+    params, root = _get_params(_get_path(tapa))
+    _walk(_get_path(tapa), os.getcwd(), params, root)
 
 
-def _walk(src, dst, params):
+def _walk(src, dst, params, root):
     env = Environment(
         loader=FileSystemLoader(src),
         undefined=StrictUndefined,
     )
-    for cur_dir, subdir_list, file_list in os.walk(src):
+    for cur_dir, subdir_list, file_list in os.walk(path.join(src, root)):
         rendered_dst_dir = path.join(dst, env.from_string(path.relpath(cur_dir, src)).render(params))
         _make_dir_if_absent(rendered_dst_dir)
         for f in file_list:
@@ -44,8 +47,23 @@ def _get_template_name(src, full_path):
     return os.path.relpath(full_path, src).replace('\\', '/')
 
 
-def _get_params():
-    return {
-        'a': 'Aaa!!!',
-        'b': 'Boo',
-    }
+def _get_params(path):
+    locals = globals()
+    result = {}
+    with open(os.path.join(path, TAPAS_FILE)) as f:
+        exec(f.read(), locals)
+
+    for arg in locals['ARGS']:
+        res = None
+        while not res:
+            try:
+                res = arg.postprocessor(input(arg.prompt))
+            except ValueError as e:
+                print(e)
+        result[arg.name] = res
+
+    return result, locals['ROOT_FOLDER']
+
+
+
+
