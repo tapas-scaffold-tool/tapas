3f7ac30e8c722ef1082ae0413dedef81d71d17e0
Kirill Sulim <kirillsulim@gmail.com>
2022-10-04 13:47:04 +0500

    Add missing package in setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from os import path
 from encodings import utf_8
 
@@ -48,7 +48,7 @@ setup(
     tests_require=[
         "parameterized",
     ],
-    packages=["tapas"],
+    packages=find_packages(where="tapas"),
     package_data={
         "tapas": [
             "tapas.version",
